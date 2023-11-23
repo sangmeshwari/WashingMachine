f66569b4b6de13177597513df34193d58d45b458
sangmeshwari <148992501+sangmeshwari@users.noreply.github.com>
2023-11-23 12:18:00 +0530

    Create driver.v

---
+++ layered_testbench/driver.v
@@ -0,0 +1,22 @@
+class driver;
+   virtual intf vif;
+   mailbox gen2driv;
+  function new(virtual intf vif,mailbox gen2driv);
+     this.vif=vif;
+     this.gen2driv=gen2driv;
+   endfunction
+   task main();
+     repeat(1)
+       begin
+         transaction trans;
+         gen2driv.get(trans);
+         vif.rst<=trans.rst;
+         vif.clk<=trans.clk;
+         vif.power<=trans.power;
+         vif.fill_water<=trans.fill_water;
+         vif.add_det<=trans.add_det;
+         trans.out<=vif.out;
+         trans.display("display from driver");
+       end
+   endtask
+ endclass
