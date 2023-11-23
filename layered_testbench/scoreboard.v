13a6288475193abfc4201f9ff3108fb21ca72b76
sangmeshwari <148992501+sangmeshwari@users.noreply.github.com>
2023-11-23 12:26:37 +0530

    Update scoreboard.v

--- layered_testbench/scoreboard.v
+++ layered_testbench/scoreboard.v
@@ -5,14 +5,26 @@ this.mon2scb=mon2scb;
 endfunction
 task main;
 transaction trans;
-repeat(1)
+  repeat(200)
 begin
 mon2scb.get(trans);
-if((trans.power==1)&&(trans.state==a)&&(trans.out==0)||(trans.fill_water==1)&&(trans.power==1)&&(trans.state==b)&& (trans.out==0)||(trans.power==1)&& (trans.add_det==1) &&(trans.state==c)&& (trans.out==0) &&(trans.power==1)&&(trans.fill_water==1)&&(trans.out==1)&&(trans.state= idle))
-$display(“Correct Result”);
+  if((trans.state==3)&&(trans.out==0)&&(trans.fill_water==1)&&(trans.add_det==1))
+   begin
+    $display("Correct Result");
+      end
+  else if((trans.state==3)&&(trans.out==0)&&(trans.fill_water==1)&&(trans.add_det==1))
+   begin
+    $display("Correct Result");
+      end
+  else if((trans.state==0)&&(trans.out==0)&&(trans.fill_water==0)&&(trans.add_det==0))
+   begin
+    $display("Correct Result");
+      end
 else
-$display(“wrong display”);
-  trans.display(“[Scoreboard]”);
+  begin
+  $display("wrong display");
+  trans.display("[Scoreboard]");
+  end
 end
 endtask
 endclass
